0008b4a441dc329d970fec129e427fed8f2444c6
Darine <tagdarinefatma@gmail.com>
2024-03-28 16:12:19 +0100

    add year, hours and minutes methods

--- script.js
+++ script.js
@@ -3,5 +3,7 @@ const dateOptionsSelectElement = document.getElementById("date-options");
 
 const date = new Date();
 const day = date.getDate();
-
-const  month = date.getMonth() + 1
+const month = date.getMonth() + 1;
+const year = date.getFullYear();
+const hours = date.getHours();
+const minutes = date.getMinutes();
